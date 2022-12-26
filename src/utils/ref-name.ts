bf783e4f0ea6eaa986b45007940a1d8bf59c54ec
tada5hi <peter.placzek1996@gmail.com>
2022-12-26 18:52:35 +0100

    fix: enhance ref-name trimming for monorepo tags

--- src/utils/ref-name.ts
+++ src/utils/ref-name.ts
@@ -5,6 +5,8 @@
  * view the LICENSE file that was distributed with this source code.
  */
 
+import semver from 'semver';
+
 export function trimRefName(name: string, prefix?: string) {
     if (
         prefix &&
@@ -12,13 +14,30 @@ export function trimRefName(name: string, prefix?: string) {
     ) {
         name = name.substring(prefix.length);
 
-        if (name[0] === '@') {
-            name = name.substring(1);
+        // monorepo tag support
+        if (
+            name[0] === '@' &&
+            semver.valid(name.substring(1))
+        ) {
+            return name.substring(1);
+        }
+
+        // monorepo tag support with version sign
+        if (
+            name[0] === '@' &&
+            name[1] === 'v' &&
+            semver.valid(name.substring(2))
+        ) {
+            return name.substring(2);
         }
     }
 
     // remove version sign
-    if (name[0] === 'v') {
+    // for semver version tag
+    if (
+        name[0] === 'v' &&
+        semver.valid(name.substring(1))
+    ) {
         name = name.substring(1);
     }
 
