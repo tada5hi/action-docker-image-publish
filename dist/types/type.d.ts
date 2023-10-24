8a073028245ade548efe9b278e435155febe3387
tada5hi <peter.placzek1996@gmail.com>
2023-10-24 20:13:57 +0200

    build: rebuild dist directory

--- dist/types/type.d.ts
+++ dist/types/type.d.ts
@@ -62,9 +62,9 @@ export type Options = {
     /**
      * Registry image tag (e.g. latest)
      *
-     * Default: ''
+     * Default: ['latest']
      */
-    registryTag: string;
+    registryTag: string[];
     /**
      * Action secret
      */
