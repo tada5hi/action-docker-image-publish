5021d8182c13b4aaaf829d2f370dd06adcf9362a
tada5hi <peter.placzek1996@gmail.com>
2023-11-01 16:28:33 +0100

    fix: remove unused dependencies + merge release-config plugins

--- release.config.js
+++ release.config.js
@@ -1,6 +1,9 @@
+const base = require('@tada5hi/semantic-release');
+
 module.exports = {
     extends: '@tada5hi/semantic-release',
     plugins: [
+        ...base.plugins,
         'semantic-release-major-tag'
     ]
 };
