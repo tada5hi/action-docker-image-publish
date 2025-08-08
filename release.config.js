42e4e251acdc2028cb0d304aa731d8db0a8b01c2
tada5hi <peter.placzek1996@gmail.com>
2025-08-08 10:59:24 +0200

    chore: switch to release-please & updated dependencies

--- release.config.js
+++
@@ -1,9 +0,0 @@
-const base = require('@tada5hi/semantic-release');
-
-module.exports = {
-    extends: '@tada5hi/semantic-release',
-    plugins: [
-        ...base.plugins,
-        'semantic-release-major-tag'
-    ]
-};
